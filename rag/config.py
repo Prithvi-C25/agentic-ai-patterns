4141782660828397bd43a293cb622db4f2c92630
Prithvi-C25 <prithvi.github@github.com>
2026-01-11 17:13:11 +0100

    creating a new rag project to test mlflow

---
+++ rag/config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    "chunk_size": 400,
+    "chunk_overlap": 80,
+    "retriever_k": 3,
+    "embeddings_model": "text-embedding-3-small",
+    "system_prompt": "You are a helpful assistant. Use the following context to answer the question. Use three sentences maximum and keep the answer concise.",
+    "llm": "gpt-5-nano",
+    "temperature": 0,
+}
---
+++ rag/rag.py
@@ -0,0 +1,31 @@
+import os
+
+import mlflow
+from langchain_community.document_loaders import ArxivLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from mlflow.genai.scorers import Correctness, ExpectationsGuidelines, RelevanceToQuery
+
+from rag.config import CONFIG
+
+mlflow.set_experiment("LangChain-RAG-MLflow")
+mlflow.langchain.autolog()
+
+# Load and process documents
+loader = ArxivLoader(
+    query="1706.03762",
+    load_max_docs=1,
+)
+
+docs = loader.load()
+print(docs[0].metadata)
+
+# Split documents into chunks
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=CONFIG["chunk_size"],
+    chunk_overlap=CONFIG["chunk_overlap"],
+)
